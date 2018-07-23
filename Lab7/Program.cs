75b75f7151e38d9ed498184703f38a5c6b3f5c93
LiegeTaco <ashketchup.pkmn2gmail.com>
2018-07-23 16:57:43 -0400

    First commit

---
+++ Lab7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+
+    class Program
+    {
+
+        static void GetValidName()
+        {
+
+
+
+        }
+
+        static void GetValidEmail()
+        {
+
+
+
+        }
+
+        static void GetValidPhone()
+        {
+
+
+
+        }
+
+        static void GetValidDate()
+        {
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+    }
+
+}
