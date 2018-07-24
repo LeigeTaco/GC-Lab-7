1c65d79455681618a0a7706cba3b18f8e302e809
williamTwomey <ashketchup.pkmn@gmail.com>
2018-07-23 20:26:15 -0400

    Hey, now there's actually code!

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Lab7
 {
@@ -13,35 +14,99 @@ namespace Lab7
         static void GetValidName()
         {
 
+            Regex isName = new Regex(@"^[A-Z]{1}[a-z]{29}$");
+            string name = Console.ReadLine();
 
+            if (isName.IsMatch(name))
+            {
 
+                Console.WriteLine("That's a name.");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("That's not a name.");
+
+            }
+           
         }
 
         static void GetValidEmail()
         {
 
+            Regex isAddress = new Regex(@"^\w{5,}@[A-z]{5,}.[A-z]{2,3}$");
+            string address = Console.ReadLine();
 
+            if (isAddress.IsMatch(address))
+            {
+
+                Console.WriteLine("That's an e-mail.");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("That's not an e-mail.");
+
+            }
 
         }
 
         static void GetValidPhone()
         {
 
+            Regex isPhone = new Regex(@"^\d{3}-\d{3}-\d{4}$");
+            string phone = Console.ReadLine();
 
+            if (isPhone.IsMatch(phone))
+            {
+
+                Console.WriteLine("That's a phone number.");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("That's not a phone number.");
+
+            }
 
         }
 
         static void GetValidDate()
         {
 
+            Regex isDate = new Regex(@"^[0123][0-9]/[01][0-9]/\d{4}$");
+            string date = Console.ReadLine();
 
+            if (isDate.IsMatch(date))
+            {
+
+                Console.WriteLine("That's a date.");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("That's not a date.");
+
+            }
 
         }
 
         static void Main(string[] args)
         {
 
-
+            GetValidName();
+            GetValidEmail();
+            GetValidPhone();
+            GetValidDate();
+            Console.WriteLine("That's all folks!");
 
         }
 
